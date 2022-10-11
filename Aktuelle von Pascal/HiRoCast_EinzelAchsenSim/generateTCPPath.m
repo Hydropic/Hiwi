7854be49fe09725db9ef8fdc34c89495543a0f1b
Penczek <penczek@ls.rub.de>
2022-10-11 08:38:00 +0200

    - Optimization without Z

--- Aktuelle von Pascal/HiRoCast_EinzelAchsenSim/generateTCPPath.m
+++ Aktuelle von Pascal/HiRoCast_EinzelAchsenSim/generateTCPPath.m
@@ -5,20 +5,17 @@ function [Position_xyz, timeLine] = generateTCPPath(optimization_values, wayPoin
 
     VelocityBoundaryCondition_x = [0, optimization_values(2,:)]
     VelocityBoundaryCondition_y = [0, optimization_values(3,:)]
-    VelocityBoundaryCondition_z = [0, optimization_values(4,:)]
 
-    AccelerationBoundaryCondition_x = [0, optimization_values(5,:)]
-    AccelerationBoundaryCondition_y = [0, optimization_values(6,:)]
-    AccelerationBoundaryCondition_z = [0, optimization_values(7,:)]
+    AccelerationBoundaryCondition_x = [0, optimization_values(4,:)]
+    AccelerationBoundaryCondition_y = [0, optimization_values(5,:)]
 
     [q_x,qd_x,qdd_x,pp_x] = quinticpolytraj(wayPoints(1,:), tpts, tvec, "VelocityBoundaryCondition", VelocityBoundaryCondition_x,"AccelerationBoundaryCondition",AccelerationBoundaryCondition_x)
     [q_y,qd_y,qdd_y,pp_y] = quinticpolytraj(wayPoints(2,:), tpts, tvec, "VelocityBoundaryCondition", VelocityBoundaryCondition_y,"AccelerationBoundaryCondition",AccelerationBoundaryCondition_y)
-    [q_z,qd_z,qdd_z,pp_z] = quinticpolytraj(wayPoints(3,:), tpts, tvec, "VelocityBoundaryCondition", VelocityBoundaryCondition_z,"AccelerationBoundaryCondition",AccelerationBoundaryCondition_z)
 
-    q_xyz = [transpose(q_x), transpose(q_y), transpose(q_z)];
-    qd_xyz = [transpose(qd_x), transpose(qd_y), transpose(qd_z)];
-    qdd_xyz = [transpose(qdd_x), transpose(qdd_y), transpose(qdd_z)];
-    pp_xyz = [transpose(pp_x), transpose(pp_y), transpose(pp_z)];
+    q_xyz = [transpose(q_x), transpose(q_y)];
+    qd_xyz = [transpose(qd_x), transpose(qd_y)];
+    qdd_xyz = [transpose(qdd_x), transpose(qdd_y)];
+    pp_xyz = [transpose(pp_x), transpose(pp_y)];
     
     
     lin_xx_x_x = linspace(0, tvec(1, end), splineDiscretization);
@@ -27,24 +24,17 @@ function [Position_xyz, timeLine] = generateTCPPath(optimization_values, wayPoin
     lin_xx_x_y = linspace(0, tvec(1, end), splineDiscretization);
     lin_yy_x_y = spline(tvec(1, :), q_xyz(:, 2), lin_xx_x_y);
 
-    lin_xx_x_z = linspace(0, tvec(1, end), splineDiscretization);
-    lin_yy_x_z = spline(tvec(1, :), q_xyz(:, 3), lin_xx_x_z);
-
     max_spline_d_x = max(qd_xyz(:,1))
     max_spline_d_y = max(qd_xyz(:,2))
-    max_spline_d_z = max(qd_xyz(:,3))
 
     min_spline_d_x = min(qd_xyz(:,1))
     min_spline_d_y = min(qd_xyz(:,2))
-    min_spline_d_z = min(qd_xyz(:,3))
 
     max_spline_dd_x = max(qdd_xyz(:,1))
     max_spline_dd_y = max(qdd_xyz(:,2))
-    max_spline_dd_z = max(qdd_xyz(:,3))
 
     min_spline_dd_x = min(qdd_xyz(:,1))
     min_spline_dd_y = min(qdd_xyz(:,2))
-    min_spline_dd_z = min(qdd_xyz(:,3))
 
     if visualizeTCPPath
         figure('units','normalized','outerposition',[0 0 1 1])
@@ -102,6 +92,6 @@ function [Position_xyz, timeLine] = generateTCPPath(optimization_values, wayPoin
         legend('X','Y','Z') 
     end
     timeLine = lin_xx_x_x; 
-    Position_xyz = [transpose(lin_yy_x_x), transpose(lin_yy_x_y), transpose(lin_yy_x_z)];
+    Position_xyz = [transpose(lin_yy_x_x), transpose(lin_yy_x_y)];
 end
 
