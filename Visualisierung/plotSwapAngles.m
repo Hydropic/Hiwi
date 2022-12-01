7727711500f2d30bf4622179d541c2dce0a1930d
Hydropic <ayman.soultana@ruhr-universitt-bochum.de>
2022-12-01 13:02:23 +0100

    Added script to visualize swap angles and added results of simulation to the right folder

---
+++ Visualisierung/plotSwapAngles.m
@@ -0,0 +1,25 @@
+function [fig] = plotSwapAngles(emiFile)
+
+%% PLOT ANGLES
+
+%READ FILES
+lineOfEmi = regexp(fileread(emiFile),'\n','split');
+whichline = find(contains(lineOfEmi,'Timestep[s] | HighestPointXYZ[m] | AngleToY[°] | Distance[m] | AngleX[°] | AngleY[°]'));
+data = dlmread(emiFile,'',whichline);
+
+%Configure Figure and plot
+fig = figure(1);
+subplot(2,1,1)
+plot(data(:,1), data(:,7))
+title('X Angle (Swap)')
+xlabel 'Timestep';
+ylabel 'XAngle'
+
+subplot(2,1,2)
+plot(data(:,1), data(:,8))
+title('Y Angle (Swap)')
+xlabel 'Timestep';
+ylabel 'YAngle'
+
+saveas(fig,'100Wasser0Gly.jpg' )
+end
