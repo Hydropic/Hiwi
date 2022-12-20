28dc5252015193b46e2aa6966eee2c33b4e06d6f
Hydropic <ayman.soultana@ruhr-universitt-bochum.de>
2022-12-20 04:41:19 +0100

    Added cubic spline to plotSwapAngles.m

--- Visualisierung/plotSwapAngles.m
+++ Visualisierung/plotSwapAngles.m
@@ -7,16 +7,29 @@ lineOfEmi = regexp(fileread(emiFile),'\n','split');
 whichline = find(contains(lineOfEmi,'Timestep[s] | HighestPointXYZ[m] | AngleToY[°] | Distance[m] | AngleX[°] | AngleY[°]'));
 data = dlmread(emiFile,'',whichline);
 
+
+timeData = data(:,1)
+
+%Calculate distance between timeintervals
+timeintervals = zeros(1,size(timeData,1) - 1);
+sizeofArray = length(timeData) - 1;
+disp(sizeofArray)
+for i = 1:sizeofArray
+    timeintervals(i) = timeData(i+1) - timeData(i);
+end
+
 %Configure Figure and plot
 fig = figure(1);
 subplot(2,1,1)
-plot(data(:,1), data(:,7))
+[spline, velocity, acceleration, ruck , time] =  splineOptimal(data(:,7),timeintervals,false);
+plot(time, spline)
 title('X Angle (Swap)')
 xlabel 'Timestep';
 ylabel 'XAngle'
 
 subplot(2,1,2)
-plot(data(:,1), data(:,8))
+[spline2, velocity, acceleration, ruck , time2] =  splineOptimal(data(:,8),timeintervals,false);
+plot(time2, spline2)
 title('Y Angle (Swap)')
 xlabel 'Timestep';
 ylabel 'YAngle'
