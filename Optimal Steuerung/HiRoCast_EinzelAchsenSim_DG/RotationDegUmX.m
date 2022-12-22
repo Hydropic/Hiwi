1f8411dfed8a79656a527bcf26988616b0a5cce9
Penczek <penczek@ls.rub.de>
2022-12-22 13:23:57 +0100

    - löschen der überflüssigen Optimale-Steuerungs-Simulationen

--- Optimal Steuerung/HiRoCast_EinzelAchsenSim_DG/RotationDegUmX.m
+++
@@ -1,8 +0,0 @@
-function rotationsmatrix = RotationDegUmX(gamma)
-
-    gamma = deg2rad(gamma);
-    
-    rotationsmatrix = [1    0                     0;
-                       0    cos(gamma)  -sin(gamma);
-                       0    sin(gamma)   cos(gamma)];
-end
--- Optimal Steuerung/HiRoCast_EinzelAchsenSim_Druckguss/debug_place.m
+++
@@ -1,7 +0,0 @@
-function debug_Place(place)
-    if 0 ~= sum(place==0)
-        fprintf('ESKALATION/ERROR in place/spline');
-        fprintf('place(%d) [%s] \n',length(place),join(string( place ), ','));
-    end
-end
-
